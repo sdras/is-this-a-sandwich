dcd016eac3a820213ad604965f9ab684908bb2c7
Sarah Drasner <sarah.drasner@gmail.com>
2020-04-04 17:59:26 -0600

    getting started building

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        is-this-a-sandwich
-      </h1>
-      <h2 class="subtitle">
-        well, is it?
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <main>
+    <app-mainbk></app-mainbk>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AppMainbk from "~/components/AppMainbk.vue";
 
 export default {
   components: {
-    Logo
+    AppMainbk
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +25,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
